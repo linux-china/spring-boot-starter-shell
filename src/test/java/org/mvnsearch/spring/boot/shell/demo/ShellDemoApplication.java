8459f11fdd6435e07165f111418c52dc4816e637
linux_china <linux_china@hotmail.com>
2016-05-24 22:02:23 +0800

    shell demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/shell/demo/ShellDemoApplication.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.spring.boot.shell.demo;
+
+import org.mvnsearch.spring.boot.shell.SpringShellApplication;
+
+/**
+ * shell demo application
+ *
+ * @author linux_china
+ */
+public class ShellDemoApplication {
+    public static void main(String[] args) {
+        SpringShellApplication.run(ShellDemoApplication.class, args);
+    }
+}
